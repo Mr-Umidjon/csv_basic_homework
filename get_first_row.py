b8ec39d770bfe0291e2f8dc1315afd7bf4689c27
Umidjon <mrumidjon2020@gmail.com>
2022-04-15 06:20:25 +0500

    Get the first row from a CSV file. 01

--- get_first_row.py
+++ get_first_row.py
@@ -1,12 +1,12 @@
-def get_first_row(data):   
-   """
-   Get the first row from a CSV file.
-    Args:
-        data(str): csv file.
-    Return:
-        list: First row.
-   """
-   
-   return
+def get_first_row(data):
+    """
+    Get the first row from a CSV file.
+     Args:
+         data(str): csv file.
+     Return:
+         list: First row.
+    """
+
+    return data.split("\n")[1].split(',')
 
 # Read the csv file
